595ab7ee799254e25f7a1f7929f7618e57330f9e
Mazen <mazen.halawi79@hotmail.com>
2020-12-08 16:07:48 +0200

    Project Setup
    Project preparation for WAW E-commerce app

---
+++ waw_app/lib/Scenes/HomeScene.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HomeScene extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ waw_app/lib/Scenes/WishlistScene.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class WishlistScene extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
