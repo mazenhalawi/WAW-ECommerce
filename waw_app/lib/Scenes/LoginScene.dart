d04e3f48339f71cc595503cb032fdab103b0644b
Mazen <mazen.halawi79@hotmail.com>
2020-12-09 10:43:41 +0200

    Setup LoginScene UI Design
    Populated LoginScene design.

--- waw_app/lib/Scenes/LoginScene.dart
+++ waw_app/lib/Scenes/LoginScene.dart
@@ -1,8 +1,156 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:waw_app/Views/Buttons/DefaultButton.dart';
+import 'package:waw_app/Views/Buttons/DefaultIconButton.dart';
+import 'package:waw_app/Views/Text/PasswordField.dart';
 
-class LoginScene extends StatelessWidget {
+class LoginScene extends StatefulWidget {
+  @override
+  _LoginSceneState createState() => _LoginSceneState();
+}
+
+class _LoginSceneState extends State<LoginScene> {
+  String _password = '';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _getHeadingTexts(context),
+            _getUsernameField(),
+            _getPasswordField(),
+            _getEmailLoginButton(),
+            Text('or'),
+            _getFacebookLoginButton(),
+            _getForgetPasswordButton(),
+            _getSignUpButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getHeadingTexts(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              'Welcome,',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10, bottom: 15),
+          child: Row(
+            children: [
+              Text('Sign in to continue'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _getUsernameField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 8,
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Username',
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.black26,
+              width: 0.5,
+              style: BorderStyle.solid,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getPasswordField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 8,
+      ),
+      child: PasswordField((value) {
+        this._password = value;
+      }),
+    );
+  }
+
+  Widget _getEmailLoginButton() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 24, bottom: 16),
+      child: Row(
+        children: [
+          Expanded(
+            child: DefaultButton(
+              title: 'Login',
+              onPressed: () {
+                print(_password);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getFacebookLoginButton() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 16,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: DefaultIconButton(
+              title: 'Login with facebook',
+              prefixIcon: Image.asset('assets/images/icon_facebook_gray.png'),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getForgetPasswordButton() {
+    return FlatButton(
+      child: Text('Forgot Password?'),
+      onPressed: () {},
+    );
+  }
+
+  Widget _getSignUpButton(BuildContext context) {
+    return FlatButton(
+      child: RichText(
+        text: TextSpan(
+            text: 'Don\'t have an account? ',
+            style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).buttonTheme.colorScheme.secondary),
+            children: [
+              TextSpan(
+                text: 'Sign Up',
+                style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: Theme.of(context).primaryColor),
+              ),
+            ]),
+      ),
+      onPressed: () {},
+    );
   }
 }
--- waw_app/lib/Utility/Constants.dart
+++ waw_app/lib/Utility/Constants.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart';
+
 const double kBUTTON_RADIUS = 30;
+
+const Color kPRIMARY_COLOR = Colors.deepPurple;
+const Color kACCENT_COLOR = Colors.deepPurpleAccent;
+
+const String kDEFAULT_FONT = 'Helvetica Neue';
