f77e5cb97dc2db555b188c39b3e949e976cf6ec2
Mazen <mazen.halawi79@hotmail.com>
2020-12-11 10:47:42 +0200

    Created Streams and Transformer to handle data

--- waw_app/lib/Blocs/HomeBloc.dart
+++ waw_app/lib/Blocs/HomeBloc.dart
@@ -1,14 +1,17 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
+import 'package:rxdart/subjects.dart';
 import 'package:waw_app/Enums/ResultStatus.dart';
 import 'package:waw_app/Managers/ConnectionManager.dart';
 import 'package:waw_app/Models/Campaign.dart';
 
+export 'package:waw_app/Models/Campaign.dart';
+
 class HomeBloc with ChangeNotifier {
   final connManager = ConnectionManager();
-  final _downloadController = StreamController<Map<String, dynamic>>();
-  final _campaignController = StreamController<List<Campaign>>();
+  final _downloadController = BehaviorSubject<List<Map<String, dynamic>>>();
+  final _campaignController = BehaviorSubject<List<Campaign>>();
 
   Stream<List<Campaign>> get campaign$ => _campaignController.stream;
 
@@ -18,14 +21,18 @@ class HomeBloc with ChangeNotifier {
         .pipe(_campaignController);
   }
 
-  void getCampaigns() async {
+  Future<void> getCampaigns() async {
     final result = await connManager.getCampaigns();
+
     if (result.status == ResultStatus.SUCCESS && result.hasData) {
       try {
-        _downloadController.add(result.data);
+        final data =
+            (result.data as List<dynamic>).cast<Map<String, dynamic>>();
+        _downloadController.sink.add(data);
         return;
       } on Exception {
         //Handle bad data received from server
+        print('getCampaigns failed');
         print(result.message ?? 'Possible bad data received from server.');
       }
     }
@@ -34,12 +41,11 @@ class HomeBloc with ChangeNotifier {
         'An unidentified error has occurred, please try again later.');
   }
 
-  StreamTransformer<Map<String, dynamic>, List<Campaign>>
+  StreamTransformer<List<Map<String, dynamic>>, List<Campaign>>
       get _mapToCampaignTransformer => StreamTransformer.fromHandlers(
-          handleData: (Map<String, dynamic> data, sink) {
-            final campaigns = (data as List<dynamic>)
-                .map((e) => Campaign.fromJSON(e))
-                .toList();
+          handleData: (List<Map<String, dynamic>> data,
+              EventSink<List<Campaign>> sink) {
+            final campaigns = data.map((e) => Campaign.fromJSON(e)).toList();
             sink.add(campaigns);
           },
           handleError: (error, stacktrace, sink) {});
--- waw_app/lib/Scenes/HomeScene.dart
+++ waw_app/lib/Scenes/HomeScene.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:waw_app/Blocs/HomeBloc.dart';
-import 'package:waw_app/Views/CampaignView.dart';
+import 'package:waw_app/Views/Alert.dart';
 
 class HomeScene extends StatefulWidget {
   static const SceneName = 'HomeScene';
@@ -25,10 +25,44 @@ class _HomeSceneState extends State<HomeScene>
 
   @override
   Widget build(BuildContext context) {
+    _bloc.getCampaigns();
     return Scaffold(
       body: Container(
         color: Color.fromRGBO(250, 247, 250, 1),
-        child: CampaignView(),
+        child: RefreshIndicator(
+          onRefresh: () => _bloc.getCampaigns(),
+          child: StreamBuilder(
+            stream: _bloc.campaign$,
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Campaign>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (snapshot.hasError) {
+                Alert(
+                        context: context,
+                        title: 'Failure',
+                        content: snapshot.error)
+                    .show();
+              } else if (!snapshot.hasData) {
+                Alert(
+                        context: context,
+                        title: 'No Data Found',
+                        content: 'No data found. Please try again.')
+                    .show();
+              } else if (snapshot.hasData) {
+                final campaigns = snapshot.data;
+                return ListView(
+                  children: campaigns.map((e) => Text(e.product.name)).toList(),
+                );
+              }
+
+              return Container();
+            },
+          ),
+        ),
       ),
     );
   }
